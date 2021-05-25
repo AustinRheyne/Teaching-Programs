fc3930af8efa7d02ec629c92e1dcf31933e032d9
Austin Rheyne <57242191+Bleakmountain50@users.noreply.github.com>
2021-05-25 00:15:00 -0400

    Added the Constants.py program which teaches students how to use constants in their code

---
+++ Constants.py
@@ -0,0 +1,26 @@
+# Example of a constant
+first_name = "Austin"
+last_name = "Rheyne"
+hair_color = "Brown"
+eye_color = "Blue"
+favorite_soup = "Chicken Noodle"
+favorite_hat = "Trucker"
+
+
+print(f"My name is {first_name} {last_name}")
+
+print(f"My hair is {hair_color}. My eyes are {eye_color}")
+
+# Challenge students to do this themseleves
+# CHALLENGE: Print out your favorite soup in a sentence
+print(f"My favorite soup is {favorite_soup}")
+print(f"My favorite hat is a {favorite_hat} hat")
+
+# Hard Challenge:
+# Create variables for your birth day, month, and year
+# Then print it out in the mm/dd/yyyy format
+birth_day = 31
+birth_month = 7
+birth_year = 2004
+
+print(f'I was born on {birth_month}/{birth_day}/{birth_year}')
